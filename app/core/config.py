f0b8ca7febdc4540a9449c8ccbfcd31585949238
Rahul Pandey <rahulpandey@Rahuls-MacBook-Pro.local>
2024-10-01 22:57:46 +0530

    Added Nvidia Endpoint API

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     OPENAI_API_KEY: str
     LLAMA_API_URL: str
+    NVIDIA_API_URL : str
     DATABASE_URL: str = "sqlite:///./test.db"
     MULTI_USER_ACCESS: bool = True
 
